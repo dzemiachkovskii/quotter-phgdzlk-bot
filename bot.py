f19fa6ac892fba222ce13b3140a171ada8a8761b
dzemiachkovskii <fennyboy2012@gmail.com>
2023-01-07 13:10:43 +0300

    delete webhook & heroku stuff

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import os
-from flask import Flask, request
-from telebot import TeleBot, types
+from telebot import TeleBot
 from image import getimg
+from TOKEN import BOT_TOKEN
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = TeleBot(BOT_TOKEN)
-server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -23,19 +21,7 @@ def txt_handler(m):
         if len(quote) < 3:
             return
         im = getimg(quote)
-        bot.send_photo(chat_id=m.chat.id, photo=im, caption="#цитата")
+        bot.send_photo(chat_id=m.chat.id, photo=im, caption=f"#цитата ({quote[2]})")
 
 
-@server.route(f'/{BOT_TOKEN}', methods=['POST'])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-APP_URL = os.environ.get('APP_URL')
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+bot.infinity_polling()
